59381c8538960d823706317b6dc606131ef89335
Andrew Craswell <ajcraswell@gmail.com>
2026-01-19 12:13:21 -0800

    Remove unused fragment

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,13 +5,11 @@ import { NotificationService } from "@/notification-service";
 export function Dashboard() {
 
   return (
-    <>
-      <div>
-        <NotificationList />
-        <Button onClick={() => {
-          NotificationService.raise({ message: "New notification raised from the [Dashboard] page" });
-        }} className="mt-4">Raise notification</Button>
-      </div>
-    </>
+    <div>
+      <NotificationList />
+      <Button onClick={() => {
+        NotificationService.raise({ message: "New notification raised from the [Dashboard] page" });
+      }} className="mt-4">Raise notification</Button>
+    </div>
   );
 }
